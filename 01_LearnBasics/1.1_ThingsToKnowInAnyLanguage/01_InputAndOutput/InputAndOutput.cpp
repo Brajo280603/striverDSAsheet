e982f2cd9fb13b29774724731ae78c41bd9655e9
Brajananda Mahata <brajanandamahato28603@gmail.com>
2022-12-01 00:34:46 +0530

    30Nov Commit

---
+++ 01_LearnBasics/1.1_ThingsToKnowInAnyLanguage/01_InputAndOutput/InputAndOutput.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s;
+    int n;
+    cout << "insert a string"<<endl;
+    cin >> s;
+    cout << "insert a integer"<<endl;
+    cin >> n;
+    cout << "the integer : " << n << endl << "the String : " << s;
+     
+    
+    return 0;
+}
