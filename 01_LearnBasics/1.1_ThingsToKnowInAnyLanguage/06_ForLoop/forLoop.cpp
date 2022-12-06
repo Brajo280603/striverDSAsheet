d3d42f2047fa013c15f25b831dd82a8ca66ec4f0
Brajananda Mahata <brajanandamahato28603@gmail.com>
2022-12-07 00:22:05 +0530

    dec 7 commit

---
+++ 01_LearnBasics/1.1_ThingsToKnowInAnyLanguage/06_ForLoop/forLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<< "the following example will print numbers from 0 to 100 using a for loop"<<endl;
+    for(int i = 0;i<=100;i++)
+    {
+        cout<<i<<endl;
+    }
+
+    return 0;
+}
---
+++ 01_LearnBasics/1.1_ThingsToKnowInAnyLanguage/07_WhileLoop/whileLoop.cpp
@@ -0,0 +1,14 @@
+#include<iostream> 
+
+using namespace std;
+
+int main(){
+    cout<< "the following example will print 0 to 100 using a while loop "<<endl;
+
+    int i =0;
+    while(i != 101)
+    {
+        cout<< i<<endl;
+        i++;
+    }
+}
